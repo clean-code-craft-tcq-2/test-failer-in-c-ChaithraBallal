4cbc6ca24872c5824cf97bd370bea98c554fa664
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-02-01 23:30:32 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -45,7 +45,6 @@ int main()
     alertInCelcius(303.6);
     alertInCelcius(650);
     alertInCelcius(400.5);
-    assert(alertInCelcius_main(400.5) == 204.7f);
     printf("%d alerts failed.\n", alertFailureCount);
     assert(alertFailureCount == 3);
     printf("All is well (maybe!)\n");
